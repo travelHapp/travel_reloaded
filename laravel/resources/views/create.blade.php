6dadfc435f9fc1dfbacb5954098f655fcef93166
mgblanco10 <mgblanco1001@gmail.com>
2023-08-22 09:31:23 +0200

    alerts form

--- laravel/resources/views/create.blade.php
+++ laravel/resources/views/create.blade.php
@@ -3,18 +3,30 @@
 
 <div>
     <h2>Crear destino</h2>
+
+    @if ($errors->any())
+    <div class="alert alert-danger mt-2">
+        <strong>Lo lamento algo fue mal...</strong> <br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form action="{{route('happy_travel.store')}}" method="POST" class="create-dest">
     @csrf
     <div class="col-mb-3 position-relative">
         <label for="validationTooltip01" class="form-label">Título</label>
-        <input type="text" name="name" class="form-control" id="validationTooltip01" placeholder="Indica el nombre del destino" required>
+        <input type="text" name="name" class="form-control" id="validationTooltip01" placeholder="Indica el nombre del destino">
         <!-- <div class="valid-tooltip">
             Looks good!
         </div> -->
     </div>
     <div class="col-mb-3 position-relative">
         <label for="validationTooltip01" class="form-label">Ubicación</label>
-        <input type="text" name="location" class="form-control" id="validationTooltip01" placeholder="Indica el destino" required>
+        <input type="text" name="location" class="form-control" id="validationTooltip01" placeholder="Indica el destino">
         <!-- <div class="valid-tooltip">
             Looks good!
         </div> -->
@@ -22,13 +34,13 @@
 
     <div class="mb-3">
     <label for="validationTextarea" class="form-label">Imagen</label>
-    <input type="file" name="image" class="form-control" aria-label="file example" placeholder="Sube una imagen" required>
+    <input type="file" name="image" class="form-control" aria-label="file example" placeholder="Sube una imagen">
     <div class="invalid-feedback">Example invalid form file feedback</div>
   </div>
 
   <div class="mb-3">
     <label for="validationTextarea" class="form-label">¿Por qué quieres viajar allí?</label>
-    <textarea class="form-control" name="description" id="validationTextarea" placeholder="Cuéntanos por qué te gusta este destino" required></textarea>
+    <textarea class="form-control" name="description" id="validationTextarea" placeholder="Cuéntanos por qué te gusta este destino"></textarea>
     <!-- <div class="invalid-feedback">
       Please enter a message in the textarea.
     </div> -->
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -3,6 +3,8 @@
 
 <div>
     <h1>Hola soy Flor</h1>
+    <h2>Aquí es donde van las cards de los destinos</h2>
+    <a href="{{ route('happy_travel.create') }}"  class="btn btn-primary">Page Introducir destino</a>
 
     </div>
 @endsection
