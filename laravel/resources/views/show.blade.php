e222ef6dcc0b27e9253443914962ba4d2fd64e68
florienborg <flornbg014@gmail.com>
2023-08-23 12:34:47 +0200

    estilos responsive show

--- laravel/resources/views/show.blade.php
+++ laravel/resources/views/show.blade.php
@@ -9,9 +9,11 @@
         <div class="col-md-6 show-text">
             <img class="icon" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono home">
             <img class="icon" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono perfil">
-            <h2>{{ $travel->name }}</h2>
-            <h3>{{ $travel->location }}</h3>
-            <p>{{ $travel->description }}</p>
+        <div class="container-text">
+            <h2 class="travel-name">{{ $travel->name }}</h2>
+            <h3 class="travel-location">{{ $travel->location }}</h3>
+        </div>
+            <p class="travel-description">{{ $travel->description }}</p>
         </div>
     </div>
 </div>
