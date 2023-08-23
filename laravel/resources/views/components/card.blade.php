ded7efb931fa53fc5695065e5b39b35e0f92ef1c
genesis-nf <genefernandz@gmail.com>
2023-08-24 01:05:47 +0200

    Public función store de la carpeta TravelController.php modificado para cargar imagen

--- laravel/resources/views/components/card.blade.php
+++ laravel/resources/views/components/card.blade.php
@@ -1,9 +1,10 @@
 <a href="{{ route('happy_travel.show', ['happy_travel' => $travel->id]) }}" class="card-link">
-  <div class="card" style="width: 17rem;">
+<div class="card" style="width: 17rem;">
     <img class="card-img-top" src="{{ $travel->image }}" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">{{ $travel->name }}</h5>
-      <p class="card-text">{{ $travel->location }}</p>
+        <h5 class="card-title">{{ $travel->name }}</h5>
+        <p class="card-text">{{ $travel->location }}</p>
     </div>
-  </div>
+</div>
 </a>
+
