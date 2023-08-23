ccad60d59b7b7d558623e02dc2c1acd1cce3bebd
florienborg <flornbg014@gmail.com>
2023-08-23 17:42:34 +0200

    centrado cards, estilo show responsive

--- laravel/resources/views/show.blade.php
+++ laravel/resources/views/show.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
     <div class="show-container">
-        <div class="col-md-6">
             <img class="show-image" src="{{ asset($travel->image) }}" alt="Imagen del destino">
-        </div>
-        <div class="col-md-6 show-text">
-            <img class="icon" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono home">
-            <img class="icon" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono perfil">
-        <div class="container-text">
-            <h2 class="travel-name">{{ $travel->name }}</h2>
-            <h3 class="travel-location">{{ $travel->location }}</h3>
-        </div>
+        <div class="text-container">
+            <div class="titles-container">
+                <h2 class="travel-name">{{ $travel->name }}</h2>
+                <h3 class="travel-location">{{ $travel->location }}</h3>
+            </div>
             <p class="travel-description">{{ $travel->description }}</p>
+        </div>
+        <div class="icon-container">
+            <img class="icon-edit" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono editar">
+            <img class="icon-delete" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono borrar">
         </div>
     </div>
 </div>
