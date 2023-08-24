adef1a85ca0e596455a900905fed33ddf8bc247b
mgblanco10 <mgblanco1001@gmail.com>
2023-08-24 07:41:12 +0200

    icon-Info card redirect and ubication of card destination

--- laravel/resources/views/components/card.blade.php
+++ laravel/resources/views/components/card.blade.php
@@ -1,4 +1,9 @@
 <div class="card" style="width: 17rem;">
+    @auth
+    <a href="{{ route('happy_travel.show', ['happy_travel' => $travel->id]) }}" class="card-link">
+        <img class="icon-info" src="{{ asset('assets/Info-icon.svg') }}" alt="icono info">
+    </a>
+    @endauth
     <img class="card-img-top" src="{{ $travel->image }}" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">{{ $travel->name }}</h5>
@@ -12,10 +17,5 @@
         </a>
         @endif
     </div>
-    @auth
-    <a href="{{ route('happy_travel.show', ['happy_travel' => $travel->id]) }}" class="card-link">
-        <img class="icon-info" src="{{ asset('assets/Info-icon.svg') }}" alt="icono info">
-    </a>
-    @endauth
 </div>
 
