a0a012f119c221b6eeacf403d284318403045be8
mgblanco10 <mgblanco1001@gmail.com>
2023-08-24 07:01:16 +0200

    condicionales para visualizacion de iconos e card segun el caso

--- laravel/resources/views/components/card.blade.php
+++ laravel/resources/views/components/card.blade.php
@@ -1,10 +1,21 @@
-<a href="{{ route('happy_travel.show', ['happy_travel' => $travel->id]) }}" class="card-link">
 <div class="card" style="width: 17rem;">
     <img class="card-img-top" src="{{ $travel->image }}" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">{{ $travel->name }}</h5>
         <p class="card-text">{{ $travel->location }}</p>
     </div>
+    <div class="icon-container">
+        @if(Auth::check() && $travel->user_id === Auth::user()->id)
+        <img class="icon-edit" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono editar">
+        <a id="deleteLink" href="#" data-toggle="modal" data-target="#deleteModal">
+            <img class="icon-delete" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono borrar">
+        </a>
+        @endif
+    </div>
+    @auth
+    <a href="{{ route('happy_travel.show', ['happy_travel' => $travel->id]) }}" class="card-link">
+        <img class="icon-info" src="{{ asset('assets/Info-icon.svg') }}" alt="icono info">
+    </a>
+    @endauth
 </div>
-</a>
 
--- laravel/resources/views/show.blade.php
+++ laravel/resources/views/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="show-container">
-            <img class="show-image" src="{{ asset($travel->image) }}" alt="Imagen del destino">
+        <img class="show-image" src="{{ asset($travel->image) }}" alt="Imagen del destino">
         <div class="text-container">
             <div class="titles-container">
                 <h2 class="travel-name">{{ $travel->name }}</h2>
@@ -12,10 +12,12 @@
             <p class="travel-description">{{ $travel->description }}</p>
         </div>
         <div class="icon-container">
-            <img class="icon-edit" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono editar">
-            <a id="deleteLink" href="#" data-toggle="modal" data-target="#deleteModal">
-                <img class="icon-delete" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono borrar">
-            </a>
+            @if(Auth::check() && $travel->user_id === Auth::user()->id)
+                <img class="icon-edit" src="{{ asset('assets/Edit-icon.svg') }}" alt="icono editar">
+                <a id="deleteLink" href="#" data-toggle="modal" data-target="#deleteModal">
+                    <img class="icon-delete" src="{{ asset('assets/Delete-icon.svg') }}" alt="icono borrar">
+                </a>
+            @endif
         </div>
     </div>
 </div>
