478305598045e6d9ae27f2789587eaf1c51c1754
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-08-28 14:50:33 +0200

    eliminar carpeta laravel

--- laravel/resources/js/search-form.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchIcon = document.getElementById('search-icon');
-    const searchForm = document.getElementById('search-form');
-
-    searchIcon.addEventListener('click', function() {
-        searchForm.submit();
-    });
-});
--- laravel/resources/views/components/modal.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="modal">
-<link href="{{ asset('css/modal.blade.css') }}" rel="stylesheet">
-    <div class="text-modal">¿Quieres eliminar este destino?</div>
-    <button class="btn btn-primary">Aceptar<button>
-    <button class="btn btn-primary">Cancelar<button>
-</div>
