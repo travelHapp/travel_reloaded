fb6fa85b1bbc2eb933a369f3e9506881b248ec15
dkhurlop <denisekhurlopian@gmail.com>
2023-08-29 14:58:22 +0200

    create destination

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CrearDestino from './pages/create-destination/Create-destination';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CrearDestino/>
     </div>
   );
 }
