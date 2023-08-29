c74afcc7ff55f2a9c9a55c3e4940b8f772d90d75
dkhurlop <denisekhurlopian@gmail.com>
2023-08-29 16:05:18 +0200

    Edit form

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 
 import './App.css';
-import CrearDestino from './pages/create-destination/Create-destination';
-import ButtonReusable from './components/buttonReusable/ButtonReusable';
+// import CrearDestino from './pages/create-destination/Create-destination';
+// import ButtonReusable from './components/buttonReusable/ButtonReusable';
+// import EditarDestino from './pages/edit-destination/Edit-destination';
 
 function App() {
-  const onClick1 = () => {
-    console.log('click 1');
-  };
-  const onClick2 = () => {
-    console.log('click 2');
-  };
+  // const onClick1 = () => {
+  //   console.log('click 1');
+  // };
+  // const onClick2 = () => {
+  //   console.log('click 2');
+  // };
   
   return (
    <div className="App">
-   <CrearDestino/>
-   <ButtonReusable text= "Aceptar" id={1} onClick={onClick1} nameClass= "btn-aceptar"/>
-   <ButtonReusable text= "Cancelar" id={2} onClick={onClick2} nameClass= "btn-cancelar"/>
    
    </div> 
   );
