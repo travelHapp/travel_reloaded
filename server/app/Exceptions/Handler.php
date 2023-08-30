f3f6faef1860767684ac65941d1e9c06a61e30fb
florienborg <flornbg014@gmail.com>
2023-08-30 12:35:48 +0200

    cambio en handler

--- server/app/Exceptions/Handler.php
+++ server/app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Throwable;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -43,6 +44,10 @@ class Handler extends ExceptionHandler
 {
     if ($exception instanceof ModelNotFoundException){
         return response()->json(["res" => false, "error" => "Error modelo no encontrado"], 400);
+    }
+
+    if ($exception instanceof RouteNotFoundException){
+        return response()->json(["res" => false, "error" => "No tiene permisos para acceder a esta ruta "], 401);
     }
     
     return parent::render($request, $exception);
