48c602df164a1b072435ba8f5fb14b3110cb6e3e
dkhurlop <denisekhurlopian@gmail.com>
2023-08-31 14:42:30 +0200

    adicion de headers

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\TravelController;
