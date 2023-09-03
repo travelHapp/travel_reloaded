b3578fb8ce09638e6eff87c240a950bd64b54f81
dkhurlop <denisekhurlopian@gmail.com>
2023-09-03 15:53:02 +0200

    arreglo error servidor.

--- client/src/service/AuthService.jsx
+++ client/src/service/AuthService.jsx
@@ -1,39 +1,39 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api/auth';
+// const BASE_URL = 'http://localhost:8000/api/auth';
 
-const AuthService = {
-  register: async (userData) => {
-    try {
-      const response = await axios.post(`${BASE_URL}/register`, userData);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+// const AuthService = {
+//   register: async (userData) => {
+//     try {
+//       const response = await axios.post(`${BASE_URL}/register`, userData);
+//       return response.data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   },
 
-  login: async (credentials) => {
-    try {
-      const response = await axios.post(`${BASE_URL}/login`, credentials);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+//   login: async (credentials) => {
+//     try {
+//       const response = await axios.post(`${BASE_URL}/login`, credentials);
+//       return response.data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   },
 
-  logout: async () => {
-    try {
-      // Aquí puedes realizar la lógica necesaria para cerrar la sesión del usuario, como eliminar el token de autenticación, etc.
-      // Puedes realizar una solicitud al backend si es necesario.
+//   logout: async () => {
+//     try {
+//       // Aquí puedes realizar la lógica necesaria para cerrar la sesión del usuario, como eliminar el token de autenticación, etc.
+//       // Puedes realizar una solicitud al backend si es necesario.
       
-      // Ejemplo de borrado de token en localStorage:
-      localStorage.removeItem('auth_token');
+//       // Ejemplo de borrado de token en localStorage:
+//       localStorage.removeItem('auth_token');
       
-      // Puedes realizar otras acciones necesarias para cerrar la sesión.
-    } catch (error) {
-      throw error;
-    }
-  },
-};
+//       // Puedes realizar otras acciones necesarias para cerrar la sesión.
+//     } catch (error) {
+//       throw error;
+//     }
+//   },
+// };
 
-export default AuthService;
+// export default AuthService;
--- client/src/service/service.jsx
+++ client/src/service/service.jsx
@@ -1,72 +1,17 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api';
-
 const apiService = {
-  getAllDestinations: async () => {
+  fetchData: async () => {
     try {
-      const authToken = localStorage.getItem('auth_token');
+      const authToken = localStorage.getItem('auth_token'); // Obtener el token almacenado en localStorage
       const headers = {
         Authorization: `Bearer ${authToken}`,
       };
-      const response = await axios.get(`${BASE_URL}`, { headers });
+      const response = await axios.get('http://localhost:8000/api', { headers });
       return response.data;
     } catch (error) {
       throw error;
     }
-  },
-  
-  createDestination: async (newDestinationData) => {
-    try {
-      const authToken = localStorage.getItem('auth_token');
-      const headers = {
-        Authorization: `Bearer ${authToken}`,
-      };
-      const response = await axios.post(`${BASE_URL}`, newDestinationData, { headers });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  getDestinationById: async (Id) => {
-    try {
-      const authToken = localStorage.getItem('auth_token');
-      const headers = {
-        Authorization: `Bearer ${authToken}`,
-      };
-      const response = await axios.get(`${BASE_URL} ${Id}`, { headers });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  updateDestination: async (Id, updatedDestinationData) => {
-    try {
-      const authToken = localStorage.getItem('auth_token');
-      const headers = {
-        Authorization: `Bearer ${authToken}`,
-      };
-      const response = await axios.put(`${BASE_URL}${Id}`, updatedDestinationData, { headers });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  deleteDestination: async (Id) => {
-    try {
-      const authToken = localStorage.getItem('auth_token');
-      const headers = {
-        Authorization: `Bearer ${authToken}`,
-      };
-      const response = await axios.delete(`${BASE_URL} ${Id}`, { headers });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+  }
 };
-
 export default apiService;
