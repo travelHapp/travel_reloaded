e5f376101cc4a6784f43c188c8c9f92215896621
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-09-04 01:57:30 +0200

    codigo

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Nav from '../../components/nav/Nav';
 import DestinationCard from '../../components/destination-card/Card';
 import apiService from '../../service/service';
@@ -29,13 +30,14 @@ const Home = () => {
           <p>Cargando destinos...</p>
         ) : (
           destinations.map(travel => (
-            <div className="destination-card">
-              <DestinationCard
-                key={travel.id}
-                travel={travel}
-                imageUrl={imageUrl}
-                isAuthenticated={false} 
-              />
+            <div className="destination-card" key={travel.id}>
+              <Link to={`/destination/${travel.id}`}>
+                <DestinationCard
+                  travel={travel}
+                  imageUrl={imageUrl}
+                  isAuthenticated={false}
+                />
+              </Link>
             </div>
           ))
         )}
