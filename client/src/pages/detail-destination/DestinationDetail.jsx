3c363e8dea3dd84e9a026064b63a61483b9f7c07
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-09-07 04:51:27 +0200

    codigo

--- client/src/pages/detail-destination/DestinationDetail.jsx
+++ client/src/pages/detail-destination/DestinationDetail.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { useParams} from 'react-router-dom';
-import { fetchCardDetails } from '../../service/CardDetail';
+import { useParams } from 'react-router-dom';
+import { fetchCardDetails } from '../../service/fetchCardDetails.jsx';
 import '../../pages/detail-destination/DestinationDetail.css';
 import editIcon from '../../assets/images/Edit-icon.svg'
 import deleteIcon from '../../assets/images/Delete-icon.svg';
 import Modal from '../../components/modal/Modal';
-import { deleteTravel } from '../../service/ApiDeleteTravel'; 
+import { deleteTravel } from '../../service/ApiDeleteTravel';
 
 export default function DestinationDetail() {
   const { id } = useParams();
   const [travel, setDetails] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-
   useEffect(() => {
     const fetchDetails = async () => {
-      const travelDetails = await fetchCardDetails(id); 
-      setDetails(travelDetails); 
+      try {
+        const token = localStorage.getItem('token');
+        const travelDetails = await fetchCardDetails(id, token);
+        setDetails(travelDetails);
+        // Imprime la URL de la imagen en la consola
+        console.log('URL de la imagen:', `http://127.0.0.1:8000/${travelDetails.image}`);
+      } catch (error) {
+        console.error('Error fetching card details:', error);
+      }
     };
 
     fetchDetails();
@@ -29,28 +35,27 @@ export default function DestinationDetail() {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-      
+
   const handleDelete = async () => {
     try {
       console.log('Eliminando elemento con ID:', id);
-  
-      await deleteTravel(id); 
-  
-  
+
+      await deleteTravel(id);
     } catch (error) {
       console.error('Error al eliminar el elemento:', error);
     }
   };
 
-
   return (
     <div className="container">
       <div className="show-container">
-        <img
-          className="show-image"
-          src={`http://127.0.0.1:8000/${travel.image}`}
-          alt="Imagen del destino"
-        />
+        {travel.image && (
+          <img
+            className="show-image"
+            src={`http://127.0.0.1:8000/${travel.image}`}
+            alt="Imagen del destino"
+          />
+        )}
         <div className="text-container">
           <div className="titles-container">
             <h2 className="travel-name">{travel.name}</h2>
@@ -58,11 +63,8 @@ export default function DestinationDetail() {
           </div>
           <p className="travel-description">{travel.description}</p>
         </div>
-        <div className="icon-container"> 
-          {/* <Link to={`/edit/${details.id}`}> */}
-            <img className="icon-edit" src={editIcon} alt="icono editar" />
-          {/* </Link> */}
-
+        <div className="icon-container">
+          <img className="icon-edit" src={editIcon} alt="icono editar" />
           <img className="icon-delete" src={deleteIcon} alt="icono borrar" onClick={handleDeleteClick} />
         </div>
       </div>
